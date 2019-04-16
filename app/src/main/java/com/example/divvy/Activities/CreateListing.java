383750622817aa5a92e166b232a37497b9a95c2c
Jesus Garnica <gtechy100@gmail.com>
2019-04-15 19:17:07 -0700

    updated structure
    Models and activities added.

--- app/src/main/java/com/example/divvy/Activities/CreateListing.java
+++ app/src/main/java/com/example/divvy/Activities/CreateListing.java
@@ -1,13 +1,15 @@
-package com.example.divvy;
+package com.example.divvy.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.divvy.R;
+
+public class CreateListing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_listing);
     }
 }
--- app/src/main/java/com/example/divvy/Activities/MainActivity.java
+++ app/src/main/java/com/example/divvy/Activities/MainActivity.java
@@ -1,13 +1,15 @@
-package com.example.divvy;
+package com.example.divvy.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreateListing extends AppCompatActivity {
+import com.example.divvy.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_listing);
+        setContentView(R.layout.activity_main);
     }
 }
