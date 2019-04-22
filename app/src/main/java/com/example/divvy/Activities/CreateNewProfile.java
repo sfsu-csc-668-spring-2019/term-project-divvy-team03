a95098ca4db85d4909d645fd6b1f98dff4adac1b
Jesus Garnica <gtechy100@gmail.com>
2019-04-22 06:26:23 -0700

    WIP

---
+++ app/src/main/java/com/example/divvy/Activities/CreateNewProfile.java
@@ -0,0 +1,15 @@
+package com.example.divvy.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.divvy.R;
+
+public class CreateNewProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_profile);
+    }
+}
