d25eccca95aafca8bc14c8741d0db1c1c6138b85
Jesus Garnica <gtechy100@gmail.com>
2019-04-23 15:42:23 -0700

    Renamed Activities Folder
    New Refactor added.

--- app/src/main/java/com/example/divvy/Controllers/CreateListing.java
+++ app/src/main/java/com/example/divvy/Controllers/CreateListing.java
@@ -1,15 +1,15 @@
-package com.example.divvy.Activities;
+package com.example.divvy.Controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.divvy.R;
 
-public class CreateNewProfile extends AppCompatActivity {
+public class CreateListing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new_profile);
+        setContentView(R.layout.activity_create_listing);
     }
 }
