908fbc24facddc261d1072d9f0e8c3fd9c42c951
Alex Ha <aha2@mail.sfsu.edu>
2019-04-23 14:43:38 -0700

    started listing model

--- app/src/main/java/com/example/divvy/Listing.java
+++ app/src/main/java/com/example/divvy/Listing.java
@@ -1,18 +1,78 @@
 package com.example.divvy;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.util.Log;
 
-class Listing {
-    String listingName;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Listing {
+    String title;
     String desc;
-    int numGroup;
-    int id;
+    int capacity;
+    //change to User object when created
+    String owner;
+    String status;
+    int listingid;
 
-    Listing(){
-        listingName = "";
-        desc = "No desc";
-        numGroup = -1;
-        id = -1;
+    public Listing(String title, String desc, int capacity, String owner, String status, int listingid) {
+        this.title = title;
+        this.desc = desc;
+        this.capacity = capacity;
+        this.owner = owner;
+        this.status = status;
+        this.listingid = listingid;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public String getOwner() {
+        return owner;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public int getListingid() {
+        return listingid;
+    }
+    public void setListingid(int listingid) {
+        this.listingid = listingid;
+    }
+    public String sendData(){
+        JSONObject data = new JSONObject();
+        try {
+            data.put("listingid", listingid);
+            data.put("title", title);
+            data.put("desc", desc);
+            data.put("capacity",capacity);
+            //change to owner_id
+            data.put("owner", owner);
+            data.put("status", status);
+        }
+        catch(JSONException e) {
+            Log.d("Listing JSON", "Error adding data to Listing JSON");
+        }
+        return data.toString();
     }
 }
