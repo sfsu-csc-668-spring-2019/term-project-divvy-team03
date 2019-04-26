6385b80194140d35eb78b1ffbe754a47e9ec13de
Alex Ha <aha2@mail.sfsu.edu>
2019-04-25 19:14:19 -0700

    started review model

---
+++ app/src/main/java/com/example/divvy/Controllers/Review.java
@@ -0,0 +1,13 @@
+package com.example.divvy.Controllers;
+
+public class Review {
+    int reviewid;
+    double rating;
+    String owner;
+    String comment;
+    String targetUser;
+
+    public Review(){
+
+    }
+}
