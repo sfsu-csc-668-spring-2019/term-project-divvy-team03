3ceae2588f8fbf956810149df3a97ef06aca2b20
westune <moiseslorenzo13@gmail.com>
2019-04-27 14:53:09 -0700

    client side of messaging

--- app/src/main/java/com/example/divvy/Controllers/MessagingActivity.java
+++ app/src/main/java/com/example/divvy/Controllers/MessagingActivity.java
@@ -1,10 +1,10 @@
 package com.example.divvy.Controllers;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,11 +13,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.divvy.R;
+import com.example.divvy.models.ChatBoxAdapter;
+import com.example.divvy.models.Message;
+import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
 
 
 public class MessagingActivity extends AppCompatActivity {
@@ -30,7 +38,10 @@ public class MessagingActivity extends AppCompatActivity {
     private Socket socket;
     private String username;
     private URI uri;
-    private int listing_id;
+    private String listing_id;
+    private List<Message> messageList;
+    private ChatBoxAdapter chatBoxAdapter;
+    private Emitter.Listener messageListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +52,18 @@ public class MessagingActivity extends AppCompatActivity {
         username = getIntent().getExtras().getString(MainActivity.USERNAME);
     }
 
+    @Override
+    protected void onDestroy() {
+        socket.disconnect();
+        socket.close();
+        super.onDestroy();
+    }
+
     private boolean setUpSocket(){
         try{
             socket = IO.socket(uri);
             socket.connect();
-
+            socket.on("message", messageListener);
             return true;
         }catch(URISyntaxException e){
             e.printStackTrace();
@@ -59,6 +77,9 @@ public class MessagingActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.chat_box);
         addImageButton = findViewById(R.id.add_image_button);
         listingName = findViewById(R.id.listing_name);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
     private void setUpListeners(){
@@ -72,15 +93,35 @@ public class MessagingActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("SEND BUTTON", "CLICKED");
+                if(textField.getText().toString().trim() != ""){
+                    socket.emit(listing_id, "message", textField.getText().toString().trim(), username);
+                    textField.setText("");
+                }
             }
         });
+
+        messageListener = new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                JSONObject data = (JSONObject) args[0];
+                try{
+                    String username = data.getString("username");
+                    String message = data.getString("message");
+                    URL imageUrl = new URL(data.getString("imageUrl"));
+                    Message m = new Message(message, username, imageUrl);
+                    messageList.add(m);
+                    chatBoxAdapter = new ChatBoxAdapter(messageList);
+                    chatBoxAdapter.notifyDataSetChanged();
+                    recyclerView.setAdapter(chatBoxAdapter);
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }catch (java.net.MalformedURLException e){
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
-    @Override
-    protected void onDestroy() {
-        socket.disconnect();
-        socket.close();
-        super.onDestroy();
-    }
+
+
 }
--- app/src/main/java/com/example/divvy/models/ChatBoxAdapter.java
+++ app/src/main/java/com/example/divvy/models/ChatBoxAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.divvy.Controllers.MessagingActivity;
 import com.example.divvy.R;
 
 import java.util.List;
