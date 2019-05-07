22af689475f1c8a2ff3611cd4c907c130483e478
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2019-05-07 15:12:58 -0700

    fixed listings

--- SavvyAPI/app.js
+++ SavvyAPI/app.js
@@ -4,8 +4,8 @@ var cors = require('cors')
 const app = express()
 const morgan = require('morgan')
 http = require('http'),
-server = http.createServer(app),
-io = require('socket.io').listen(server);
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 const bodyParser = require('body-parser')
 
 app.use(cors())
@@ -33,45 +33,42 @@ app.use(listings)
 //socket config
 io.on('connection', (socket) => {
 
-console.log('user connected')
+    console.log('user connected')
 
-socket.on('join', function(userNickname) {
+    socket.on('join', function(userNickname) {
 
-        console.log(userNickname +" : has joined the chat "  );
+        console.log(userNickname + " : has joined the chat ");
 
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+        socket.broadcast.emit('userjoinedthechat', userNickname + " : has joined the chat ");
     })
 
 
-socket.on('messagedetection', (senderNickname,messageContent) => {
+    socket.on('messagedetection', (senderNickname, messageContent) => {
 
-       //log the message in console 
+        //log the message in console 
 
-       console.log(senderNickname+" : " +messageContent)
+        console.log(senderNickname + " : " + messageContent)
 
-      //create a message object 
+        //create a message object 
 
-      let  message = {"message":messageContent, "senderNickname":senderNickname}
+        let message = { "message": messageContent, "senderNickname": senderNickname }
 
-       // send the message to all users including the sender  using io.emit() 
+        // send the message to all users including the sender  using io.emit() 
 
-      io.emit('message', message )
+        io.emit('message', message)
 
-      })
+    })
 
-socket.on('disconnect', function() {
+    socket.on('disconnect', function() {
 
-        console.log(userNickname +' has left ')
+        //console.log(userNickname + ' has left ')
 
-        socket.broadcast.emit( "userdisconnect" ,' user has left')
+        socket.broadcast.emit("userdisconnect", ' user has left')
 
 
 
 
     })
-
-
-
 
 })
 
--- SavvyAPI/routes/listing.js
+++ SavvyAPI/routes/listing.js
@@ -3,7 +3,6 @@
  * this class will be used to handle get and post request for 
  * user related info. 
  */
-
 const express = require('express')
 const db = require('../config/db.config.js');
 const bodyParser = require("body-parser");
@@ -31,7 +30,7 @@ router.post('/newListing', (req, res) => {
     const status = "true"
     const description = req.body.description
     const username = req.body.username
-    var createProfile = "INSERT INTO Listing(listing_name, listing_status, listing_desc, listing_owner) VALUES (?, ?, ?, ?)"
+    var createProfile = "INSERT INTO Listing(name, status, description, owner) VALUES (?, ?, ?, ?)"
     db.query(createProfile, [name, status, description, username], (err, result) => {
         if (err) {
             console.log("failed to insert new listing " + err)
@@ -45,23 +44,22 @@ router.post('/newListing', (req, res) => {
 });
 
 router.get('/search', (req, res) => {
-    const like = req.body.like
-    var createquery = "SELECT * FROM Listing WHERE listing_name LIKE ? AND listing_status = true"
+    const like = req.query.like
+    var createquery = "SELECT * FROM Listing WHERE name LIKE ? AND status = true"
     db.query(createquery, ['%' + like + '%'], (err, rows, fields) => {
         if (err) {
             console.log(err)
             res.sendStatus(500)
             return
         } else {
-            console.log(result)
             res.json(rows)
         }
     })
 })
 
 router.get('/getbyowner', (req, res) => {
-    const username = req.body.username
-    var createquery = "SELECT * FROM Listing WHERE listing_owner = ?"
+    const username = req.query.username
+    var createquery = "SELECT * FROM Listing WHERE owner = ?"
     db.query(createquery, [username], (err, rows, fields) => {
         if (err) {
             console.log(err)
