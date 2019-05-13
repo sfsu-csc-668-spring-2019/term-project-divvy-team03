c7377fa9547e206de7ce7061a3941d45ac983cda
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2019-05-13 01:00:55 -0700

    changes in dependencies

--- SavvyAPI 2/app.js
+++ SavvyAPI 2/app.js
@@ -1,12 +1,12 @@
 //load our app server using express
-import express from 'express'
-import cors from 'cors'
+const express = require('express')
+var cors = require('cors')
 const app = express()
-import morgan from 'morgan'
-import http from 'http'
-const server = http.createServer(app),
+const morgan = require('morgan')
+http = require('http'),
+    server = http.createServer(app),
     io = require('socket.io').listen(server);
-import bodyParser from 'body-parser'
+const bodyParser = require('body-parser')
 
 app.use(cors())
 
@@ -24,39 +24,52 @@ global.__basedir = __dirname;
 
 //create router
 //this is important because it creates a connection to the users folder and runs the users.js folder
-const router = require('./controller/users.js')
+const router = require('./routes/users.js')
 app.use(router)
 
-const listings = require('./controller/listing.js')
+const listings = require('./routes/listing.js')
 app.use(listings)
-
-const listingGroup = require('./controller/listing_group')
-app.use(listingGroup)
-
-const rating = require('./controller/rating')
-app.use(rating)
 
 //socket config
 io.on('connection', (socket) => {
+
     console.log('user connected')
+
     socket.on('join', function(userNickname) {
+
         console.log(userNickname + " : has joined the chat ");
-        socket.broadcast.emit('user joined the chat', userNickname + " : has joined the chat ");
+
+        socket.broadcast.emit('userjoinedthechat', userNickname + " : has joined the chat ");
     })
 
+
     socket.on('messagedetection', (senderNickname, messageContent) => {
+
         //log the message in console 
+
         console.log(senderNickname + " : " + messageContent)
-            //create a message object 
+
+        //create a message object 
+
         let message = { "message": messageContent, "senderNickname": senderNickname }
-            // send the message to all users including the sender  using io.emit() 
+
+        // send the message to all users including the sender  using io.emit() 
+
         io.emit('message', message)
+
     })
 
     socket.on('disconnect', function() {
-        console.log('user has left ')
+
+        //console.log(userNickname + ' has left ')
+
         socket.broadcast.emit("userdisconnect", ' user has left')
+
+
+
+
     })
+
 })
 
 app.get('/', function(req, res) {
@@ -66,5 +79,5 @@ app.get('/', function(req, res) {
 
 //for local uncomment this 
 server.listen(3000, () => {
-    console.log("Server is up and listening on 3000...")
+    console.log("Server is up and listening on 3003...")
 })
